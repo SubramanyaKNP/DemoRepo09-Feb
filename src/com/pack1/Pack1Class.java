e4a57f6d478878a38c937f99c04de989d4170ebb
subramanya knp <subramanyaknp@gmail.com>
2018-02-09 18:58:34 +0530

    Its my first commit

---
+++ src/com/pack1/Pack1Class.java
@@ -0,0 +1,13 @@
+package com.pack1;
+
+public class Pack1Class {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("class 1 inside pack1");
+
+
+	}
+
+}
---
+++ src/com/pack2/Pack2Class.java
@@ -0,0 +1,10 @@
+package com.pack2;
+
+public class Pack2Class {
+
+	public static void main(String[] args) {
+
+		System.out.println("pack 2 class");
+	}
+
+}
